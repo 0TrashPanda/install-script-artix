31faced9e62675234f18165da5f40cb47dd58fca
TheTreesee <thetreesee@gmail.com>
2022-11-16 09:13:08 +0100

    passwd_check

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
-test
-
-test() {
-    echo "Hello World"
+passwd_error_check() {
+    if [ $? -ne 0 ]; then
+        echo "Error: Passwords do not match."
+        return true
+    fi
 }
+
+# update the system
+$ = false
+while $
+do
+    sudo pacman neofetch
+    passwd_error_check
+done

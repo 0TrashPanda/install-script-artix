499801ea545a63eca0e615e95ca098ea11cd490a
0TrashPanda <jonah.jvdb@gmail.com>
2022-11-19 22:14:29 +0100

    --noconfirm

--- artix_postinstall.sh
+++ artix_postinstall.sh
@@ -15,7 +15,7 @@ re() {
 # update the system
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S git wget
+sudo pacman -S --noconfirm git wget
 
 sudo mkdir /packages
 sudo chown admin: /packages
@@ -25,7 +25,7 @@ git clone https://github.com/0TrashPanda/install-script-artix
 
 sudo wget https://github.com/archlinux/svntogit-packages/raw/packages/pacman-mirrorlist/trunk/mirrorlist -O /etc/pacman.d/mirrorlist-arch
 cat /packages/install-script-artix/assets/pacman/pacman.conf | sudo tee -a /etc/pacman.conf > /dev/null
-sudo pacman -Sy
+sudo pacman -Sy --noconfirm
 
 # pacman installs
 sudo pacman -S --noconfirm neovim neofetch htop openssh ripgrep fzf zsh mandoc tmux python-pip rust doas w3m openssh-runit cmake ufw lf bat exa btop

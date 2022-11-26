ea0aa4ea92b9340fb9db1e76312131c63c1154d0
0TrashPanda <jonah.jvdb@gmail.com>
2022-11-26 13:22:16 +0100

    fixed the su shit + more features

--- assets/scripts/minecraft.sh
+++ assets/scripts/minecraft.sh
@@ -1,27 +1,3 @@
-doas pacman -S jre-openjdk-headless jre8-openjdk-headless jre11-openjdk-headless jre17-openjdk-headless
-
-doas mkdir -p /srv/minecraft
-
-doas useradd -d /srv/minecraft steve
-
-doas chown steve /srv/minecraft
-
-su steve
-
-if command -v curl >/dev/null 2>&1; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-else
-  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-fi
-
-git clone https://github.com/anatolykopyl/doas-zsh-plugin.git /packages/doas-zsh-plugin
-
-cat /packages/install-script-artix/assets/zsh/.zshrc >> ~/.zshrc
-
-sed -i '/z4h bindkey z4h-backward-kill-word  Ctrl+Backspace     Ctrl+H/d' ~/.zshrc
-sed -i "s/command tmux -u new -A -D -t z4h/command tmux -2 -u new -A -D -t z4h/" ~/.zshrc
-
-
 mkdir /srv/minecraft/jars
 mkdir /srv/minecraft/waterfall
 mkdir /srv/minecraft/lobby
@@ -31,10 +7,24 @@ mkdir /srv/minecraft/suvival
 curl https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/290/downloads/paper-1.19.2-290.jar > /srv/minecraft/jars/paper-1192.jar
 curl https://api.papermc.io/v2/projects/waterfall/versions/1.19/builds/507/downloads/waterfall-1.19-507.jar > /srv/minecraft/jars/waterfall-19.jar
 
+cp /srv/minecraft/jars/paper-1192.jar /srv/minecraft/lobby/paper-1192.jar
+cp /srv/minecraft/jars/waterfall-19.jar /srv/minecraft/waterfall/waterfall-19.jar
+
 echo "java -Xms512M -Xmx1G -jar paper-1192.jar" > /srv/minecraft/lobby/start.sh
 chmod u+x /srv/minecraft/lobby/start.sh
 
 echo "java -jar waterfall-19.jar" > /srv/minecraft/waterfall/start.sh
 chmod u+x /srv/minecraft/waterfall/start.sh
 
-exit
+tmx () {
+    # Use -d to allow the rest of the function to run
+    tmux new-session -d -s minecraft
+    tmux new-window -n extra-window
+    # -d to prevent current window from changing
+    tmux new-window -d -n waterfall
+    tmux new-window -d -n lobby
+    tmux new-window -d -n skyblock
+    tmux new-window -d -n survival
+}
+
+echo "tmux attach-session -d -t minecraft" > /srv/minecraft/start.sh
---
+++ assets/scripts/pre_minecraft.sh
@@ -0,0 +1,9 @@
+doas pacman -S jre-openjdk-headless jre8-openjdk-headless jre11-openjdk-headless jre17-openjdk-headless
+
+doas mkdir -p /srv/minecraft
+
+doas useradd -d /srv/minecraft steve
+
+doas chown steve /srv/minecraft
+
+doas su steve #! verry popo, does not work ╚(•⌂•)╝ so i need to slit the script in two parts
